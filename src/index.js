24c199428ab6791de1e48da2c8e692590776dee2
KRSBX <firdaus.sati51@gmail.com>
2021-10-26 09:57:25 +0700

    chore: remove dependencies

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import AFrameRenderer from './components/AFrameRenderer';
+import ARProvider from './components/ARProvider';
+import Animation from './components/Animation';
+import Box from './components/Box';
+import Entity from './components/Entity';
+import Marker from './components/Marker';
+import SceneRenderer from './components/SceneRenderer';
+import Example from './Example';
+
+export {
+  AFrameRenderer,
+  ARProvider,
+  Animation,
+  Box,
+  Entity,
+  Marker,
+  SceneRenderer,
+};
+
+export default Example;
