1af00c44ffc20d214d7bd7d1ce0b4234c1e42a33
KRSBX <firdaus.sati51@gmail.com>
2021-10-28 18:30:22 +0700

    chore: deployment 2021-10-28 18:30

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,71 @@
 "use strict";
 
-var _react = _interopRequireDefault(require("react"));
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "AFrameRenderer", {
+  enumerable: true,
+  get: function get() {
+    return _AFrameRenderer.default;
+  }
+});
+Object.defineProperty(exports, "ARProvider", {
+  enumerable: true,
+  get: function get() {
+    return _ARProvider.default;
+  }
+});
+Object.defineProperty(exports, "Box", {
+  enumerable: true,
+  get: function get() {
+    return _Box.default;
+  }
+});
+Object.defineProperty(exports, "Entity", {
+  enumerable: true,
+  get: function get() {
+    return _Entity.default;
+  }
+});
+Object.defineProperty(exports, "Example", {
+  enumerable: true,
+  get: function get() {
+    return _Example.default;
+  }
+});
+Object.defineProperty(exports, "Marker", {
+  enumerable: true,
+  get: function get() {
+    return _Marker.default;
+  }
+});
+Object.defineProperty(exports, "SceneRenderer", {
+  enumerable: true,
+  get: function get() {
+    return _SceneRenderer.default;
+  }
+});
+Object.defineProperty(exports, "useARCamera", {
+  enumerable: true,
+  get: function get() {
+    return _useARCamera.default;
+  }
+});
 
-var _reactDom = _interopRequireDefault(require("react-dom"));
-
-var _Example = _interopRequireDefault(require("./components/Example"));
+var _AFrameRenderer = _interopRequireDefault(require("./components/AFrameRenderer"));
 
 var _ARProvider = _interopRequireDefault(require("./components/ARProvider"));
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var _Box = _interopRequireDefault(require("./components/Box"));
 
-_reactDom.default.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_ARProvider.default, null, /*#__PURE__*/_react.default.createElement(_Example.default, null))), document.getElementById('root'));
+var _Entity = _interopRequireDefault(require("./components/Entity"));
+
+var _Example = _interopRequireDefault(require("./components/Example"));
+
+var _Marker = _interopRequireDefault(require("./components/Marker"));
+
+var _SceneRenderer = _interopRequireDefault(require("./components/SceneRenderer"));
+
+var _useARCamera = _interopRequireDefault(require("./utils/useARCamera"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
